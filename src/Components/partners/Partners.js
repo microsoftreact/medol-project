490f2fbb1f24bd346f2b7cdd7c6707ebd118ca50
nonoteacher <nodirbektoshpulatov123345@gmail.com>
2023-06-13 14:50:51 +0500

    add to partners

--- src/Components/partners/Partners.js
+++ src/Components/partners/Partners.js
@@ -4,7 +4,37 @@ import "./Partners.css";
 function Partners({ title }) {
   return (
     <div className="partners">
-      <Title title={"ПАРТНЕРЫ"} />
+      <div className="partners-title">
+        <Title title={"ПАРТНЕРЫ"} />
+      </div>
+      <div className="partners-slider">
+        <i class="fa-solid fa-chevron-left left"></i>
+        <div className="slider-one">
+          <img src="./Images/aesculap-b-braun1 1.png" alt="" />
+        </div>
+        <div className="slider-two">
+          <img src="./Images/asp1 1.png" alt="" />
+        </div>
+        <div className="slider-three">
+          <img src="./Images/bd1 1.png" alt="" />
+        </div>
+        <div className="slider-four">
+          <img src="./Images/biosense1 1.png" alt="" />
+        </div>
+        <div className="slider-five">
+          <img src="./Images/cordis1 1.png" alt="" />
+        </div>
+        <div className="slider-six">
+          <img src="./Images/depuy-synthes-spine-jandj(1) 1.png" alt="" />
+        </div>
+        <div className="slider-seven">
+          <img src="./Images/eth 1.png" alt="" />
+        </div>
+        <div className="slider-eight">
+          <img src="./Images/ethicon-endo-surgery1 1.png" alt="" />
+        </div>
+        <i class="fa-solid fa-chevron-right right"></i>
+      </div>
     </div>
   );
 }
